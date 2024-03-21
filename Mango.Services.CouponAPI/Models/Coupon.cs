13689b4c069f880fd2fba00b76ffdd4388c3c264
Yunis <yunisah@code.edu.az>
2024-03-21 20:03:22 +0400

    Add project files.

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public int couponId { get; set; }
+        public string Couponcode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
---
+++ Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        [Key]
+        public int couponId { get; set; }
+        [Required]
+        public string Couponcode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
+
